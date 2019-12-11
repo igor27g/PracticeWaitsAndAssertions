7cec634cc28c55e35c61af94c67a3c3dd00d51c5
Igor Grabowski <aigor2789@gmail.com>
2019-12-11 12:15:06 +0100

    More test to repeat information about Actions, JS in Selenium, dropdown list

--- src/test/java/RepeatWaitsAndAssertions.java
+++ src/test/java/RepeatWaitsAndAssertions.java
@@ -3,12 +3,16 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 
@@ -17,6 +21,8 @@ public class RepeatWaitsAndAssertions {
 
     WebDriver driver;
     WebDriverWait wait;
+    JavascriptExecutor js;
+    Actions actions;
 
     String fakeStore = "http://automationpractice.com/index.php";
 
@@ -40,6 +46,9 @@ public class RepeatWaitsAndAssertions {
     String alertMessage;
 
 
+
+
+
     @BeforeEach
     public void driverSetup(){
 
@@ -51,6 +60,8 @@ public class RepeatWaitsAndAssertions {
         driver.navigate().to(fakeStore);
 
         wait = new WebDriverWait(driver,10);
+        js = (JavascriptExecutor)driver;
+        actions = new Actions(driver);
 
     }
 
@@ -94,6 +105,62 @@ public class RepeatWaitsAndAssertions {
 
     private  String getAlertMessage() {
         return wait.until(ExpectedConditions.visibilityOfElementLocated(authenticationAlert)).getText();
+    }
+
+    @Test
+    public void scrollToOrders() {
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,2500)");
+        driver.findElement(By.cssSelector("a[title='My orders']")).click();
+        Assertions.assertEquals("http://automationpractice.com/index.php?controller=authentication&back=history", driver.getCurrentUrl(), "Wrong url address");
+    }
+
+    @Test
+    public void scrollToSpecials() {
+        WebElement element = driver.findElement(By.cssSelector("a[title='Specials']"));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView(true);",element);
+        element.click();
+        Assertions.assertEquals("http://automationpractice.com/index.php?controller=prices-drop", driver.getCurrentUrl(),"Wrong url address");
+    }
+
+    @Test
+    public void selectByDropdownList() {
+        driver.findElement(By.id("contact-link")).click();
+        WebElement dropdownElement = driver.findElement(By.id("id_contact"));
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByIndex(1);
+    }
+
+    @Test
+    public void selectByDropdownList2() {
+        driver.findElement(By.id("contact-link")).click();
+        WebElement dropdownElement = driver.findElement(By.id("id_contact"));
+        Select dropdown = new Select(dropdownElement);
+        dropdown.selectByValue("1");
+    }
+
+    @Test
+    public void selectProductusingActions() {
+        List<WebElement> listProduct = driver.findElements(By.cssSelector("ul[id='homefeatured']>li"));
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,500)");
+        WebElement product2 = listProduct.get(1);
+        actions.click(product2).build().perform();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("h1[itemprop='name']"))));
+        String titleProduct = driver.findElement(By.cssSelector("h1[itemprop='name']")).getText();
+        Assertions.assertEquals("Blouse", titleProduct, "Wrong title product");
+    }
+
+
+    @Test
+    public void checkProductInCart() {
+        List<WebElement> listProduct = driver.findElements(By.cssSelector("ul[id='homefeatured']>li"));
+        ((JavascriptExecutor)driver).executeScript("window.scrollBy(0,500)");
+        WebElement product2 = listProduct.get(1);
+        actions.click(product2).build().perform();
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector("h1[itemprop='name']"))));
+        WebElement cart = driver.findElement(By.cssSelector("a[href='http://automationpractice.com/index.php?controller=order']"));
+        actions.click(cart).build().perform();
+        WebElement buttonCart = driver.findElement(By.cssSelector("a[id='button_order_cart']"));
+        Assertions.assertTrue(buttonCart.isDisplayed());
     }
 
 }
